9bcbb1877d45babebfcc14aa8c3a78904275992f
andrewsuds <andrewsuds@gmail.com>
2022-05-03 06:32:06 -0400

    Added Icon

--- app/src/main/java/com/example/inventory/service/RetrofitClient.kt
+++ app/src/main/java/com/example/inventory/service/RetrofitClient.kt
@@ -3,15 +3,18 @@ package com.example.inventory.service
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
     private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(60, TimeUnit.SECONDS)
         .build()
 
     private val retrofit : Retrofit = Retrofit.Builder()
         .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl("http://10.0.0.204:3001/")
+        .baseUrl("https://nodejs-inventory.onrender.com/")
         .client(okHttpClient)
         .build()
 
